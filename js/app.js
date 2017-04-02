ff6c5b51ddc059d69e8753c6a5e58255b6719561
Angie Vanegas <arco527@gmail.com>
2017-04-01 22:28:28 -0500

    More exercices

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 var cuadrado = function(n){
 	return console.log(n * n);
 }
@@ -53,5 +54,131 @@ do{
 } while(pago);
 
 console.log(prompt("solo me haz pagado esto " + totalPago));
+
+
+
+x = 3;
+
+while( x<= 10){
+	console.log(x);
+	x++
+}
+
+var numero=0
+for(i=0; i<=10; i++){
+ console.log(numero)
+ numero++
+}
+
+
+
+var opinion = "es un libro que quiero leer"
+var libro= {
+	titulo: "IT",
+	autor:"Steven King",
+	genero:"terror",
+	valoracion: 9,
+	tags:["Pennywise", "floating","clown", "Derry"],
+	opinion:opinion,
+};
+
+
+console.info(libro.opinion)
+
+
+var nombre= {
+	nombre:"angie",
+	apellido:"vanegas",
+	nombreCompleto:function(){
+		return (this.nombre + " " + this.apellido)
+	}
+};
+
+
+
+console.log(nombre.nombreCompleto())
+
+
+
+
+
+function player (name, attackPoints){
+	this.name=name,
+	this.attackPoints=attackPoints,
+	this.lifePoints = 100
+
+	Player.prototype.attack = function (opponent){
+		opponent.lifePoints -= this.attackPoints,
+		console.log(this.name + 'just hit ' + opponent.name),
+		opponent.getLifePoints();
+	}
+	Player.prototype.getLifePoints = function (){
+	console.log(this.name + ' has ' + this.lifePoints)
+	}
+
+}
+
+var player1 = new Player('Goku', 32 )
+var player2 = new Player('freezer', 22 )
+
+
+printInfo()
+
+player1.attack(player2)
+player2.attack(player1)
+player1.attack(player2)
+
+printInfo()
+
+function printInfo(){
+	console.log('-----------------------------------')
+	console.log(Object.getPrototypeOf(player1))
+	console.log(player1)
+	console.log(player2)
+	console.log()
+}
+
+
+
+
+function closure(nombre){
+	return function(msj){
+		return(nombre + msj)
+	};
+}
+
+var closure2 = closure("oliva");
+	
+	console.log (closure2(" es mi perrita"))
+
+
+*/
+
+
+
+var casa= {
+	dormitorio: "si",
+	llaves: "si",
+	carro: "si",
+
+	delete casa.llaves
+}
+
+
+console.log(casa)
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
